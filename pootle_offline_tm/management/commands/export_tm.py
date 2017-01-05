b4d46115e3483d07967d6b15998e59bd7cdcc397
Taras Semenenko <taras.semenenko@gmail.com>
2017-01-05 11:11:12 +0300

    Add PootleCommand arguments

--- pootle_offline_tm/management/commands/export_tm.py
+++ pootle_offline_tm/management/commands/export_tm.py
@@ -25,6 +25,7 @@ class Command(PootleCommand):
     help = "Export TM from Pootle to .tmx files."
 
     def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
         parser.add_argument(
             "--single-file",
             action="store_true",
